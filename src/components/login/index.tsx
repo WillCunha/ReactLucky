f186e96094158dd998816684a686390c33193850
WillCunha <willianfernandocunha932@gmail.com>
2025-05-24 08:37:53 -0300

    Analise do erro do Google Sign In

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -48,7 +48,7 @@ export default function LoginScreen() {
                 Alert.alert("Login cancelado.")
             }
         } catch (error) {
-            //
+            Alert.alert(JSON.stringify(error));
         }
     };
 
